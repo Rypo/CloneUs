98d46aba9e723fed6a06f515f2403c36f6c919a3
Rypo <ryan@gust.dev>
2024-02-16 17:44:35 -0600

    Switch to src-layout structure

--- setup.py
+++ setup.py
@@ -4,7 +4,9 @@ setup(
     name = 'cloneus',
     version='0.0.1',
     author='Rypo',
-    packages = find_packages(),
-    url='tbd',
+    keywords="LLM, multi-user RP finetuning, discord, AI, chatbot",
+    package_dir={"": "src"},
+    packages = find_packages(where="src"),
+    url='https://github.com/Rypo/CloneUs',
     license='MIT',
 )
