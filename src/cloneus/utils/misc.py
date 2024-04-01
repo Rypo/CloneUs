154789b22ea8263b0c6342e41796e9f2e99bc3d0
Rypo <ryan@gust.dev>
2024-04-01 17:37:00 -0500

    feat: add hermes flavor chatml setup_chat_format utility

--- src/cloneus/utils/misc.py
+++ src/cloneus/utils/misc.py
@@ -1,24 +1,26 @@
 import os
 import gc
+import typing
 import json
 import warnings
-from typing import  Optional, Any
+from dataclasses import dataclass
 
 import numpy as np
 import torch
 
 import bitsandbytes as bnb
 from peft.tuners.lora import QuantLinear
+from transformers import PreTrainedModel, PreTrainedTokenizer
 
 warnings.filterwarnings(action = "ignore", category = UserWarning, module = "torch")
 
 
 # SRC: https://github.com/unslothai/unsloth/blob/dev.2023.12/unsloth/models/_utils.py#L24
 def prepare_model_for_kbit_training(
-    model                      : Any,
+    model                      : typing.Any,
     use_gradient_checkpointing : bool = True,
-    use_reentrant              : Optional[bool] = True,
-) -> Any:
+    use_reentrant              : typing.Optional[bool] = True,
+) -> typing.Any:
     """
     Calculates where to place the gradient checkpoints given n_layers.
     We also freeze all other layers's gradients
@@ -220,3 +222,86 @@ def load_lora_custom(model, peft_config, lora_target_linear=True):
     model.print_trainable_parameters()
 
     return model, peft_config
+
+
+
+# from trl
+@dataclass
+class ChatMlXSpecialTokens:
+    """Dataclass for special tokens used in ChatML, including system, user, assistant, bos, eos, and pad tokens."""
+
+    bos_token: str = "<s>"
+    bosx_token: str = "<|im_start|>"
+    eos_token: str = "<|im_end|>"
+    pad_token: str = "</s>"
+
+    @property
+    def system(self):
+        return f"{self.bosx_token}system"
+
+    @property
+    def user(self):
+        return f"{self.bosx_token}user"
+
+    @property
+    def assistant(self):
+        return f"{self.bosx_token}assistant"
+
+    @property
+    def chat_template(self):
+        return (
+            "{% for message in messages %}"
+            f"{{{{'{self.bosx_token}' + message['role'] + '\n' + message['content'] + '{self.eos_token}' + '\n'}}}}"
+            "{% endfor %}"
+            "{% if add_generation_prompt %}"
+            f"{{{{ '{self.assistant}\n' }}}}"
+            "{% endif %}"
+        )
+
+
+FORMAT_MAPPING = {"chatmlX": ChatMlXSpecialTokens}
+
+
+def setup_chat_format_patched(
+    model: PreTrainedModel,
+    tokenizer: PreTrainedTokenizer,
+    format: typing.Optional[typing.Literal["chatmlX"]] = "chatmlX",
+    resize_to_multiple_of: typing.Optional[int] = None,
+) -> typing.Tuple[PreTrainedModel, PreTrainedTokenizer]:
+    """
+    Setup chat format by adding special tokens to the tokenizer, setting the correct format, and extending the embedding layer of the model based on the new special tokens.
+
+    Args:
+      model (`~transformers.PreTrainedModel`): The model to be modified.
+      tokenizer (`~transformers.PreTrainedTokenizer`): The tokenizer to be modified.
+      format (`Optional[Literal["chatml"]]`): The format to be set. Defaults to "chatml".
+      resize_to_multiple_of (`Optional[int]`): Number to resize the embedding layer to. Defaults to None.
+    Returns:
+      model (`~transformers.PreTrainedModel`): The modified model.
+      tokenizer (`~transformers.PreTrainedTokenizer`): The modified tokenizer.
+    """
+    # check if format available and retrieve
+    if format not in FORMAT_MAPPING:
+        raise ValueError(f"Format {format} not available. Please use one of {FORMAT_MAPPING.keys()}")
+
+    chat_format = FORMAT_MAPPING[format]()
+
+    # set special tokens and them
+    tokenizer.eos_token = chat_format.eos_token
+    tokenizer.pad_token = chat_format.pad_token
+    tokenizer.bos_token = chat_format.bos_token
+    tokenizer.add_special_tokens({"additional_special_tokens": [chat_format.bosx_token, chat_format.eos_token]})
+    # set chat format for tokenizer
+    tokenizer.chat_template = chat_format.chat_template
+
+    # resize embedding layer to a multiple of 64, https://x.com/karpathy/status/1621578354024677377
+    model.resize_token_embeddings(
+        len(tokenizer), pad_to_multiple_of=resize_to_multiple_of if resize_to_multiple_of is not None else None
+    )
+    # Make sure to update the generation config to use the new eos & bos token
+    if getattr(model, "generation_config", None) is not None:
+        model.generation_config.bos_token_id = tokenizer.bos_token_id
+        model.generation_config.eos_token_id = tokenizer.eos_token_id
+        model.generation_config.pad_token_id = tokenizer.pad_token_id
+
+    return model, tokenizer
