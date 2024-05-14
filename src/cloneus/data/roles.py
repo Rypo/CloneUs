c0cc07bd3109fd57dbad535c03adc4699a5a4a3b
Rypo <ryan@gust.dev>
2024-05-14 15:18:13 -0500

    feat: add write index function, allow author names be passed to initials creation

--- src/cloneus/data/roles.py
+++ src/cloneus/data/roles.py
@@ -11,22 +11,43 @@ import cloneus.core.paths as cpaths
 
 USERS_FILEPATH = cpaths.ROOT_DIR/'config/users.json'
 
-USER_DATA = None
+USER_INDEX = None
 
-def _read_userdata():
+def _read_user_index():
     try:
         with open(USERS_FILEPATH,'r') as f:
-            USER_DATA: dict = json.load(f)
-            return USER_DATA
+            USER_INDEX: dict = json.load(f)
+            return USER_INDEX
     except FileNotFoundError as e:
-        warnings.warn('Missing config/users.json. You must run `scripts/build.py` or otherwise create users.json to proceed.')
-        return placeholder_userdata(False)
+        raise FileNotFoundError('Missing config/users.json. You must run `scripts/build.py` or otherwise create users.json to proceed.')
+
+def write_user_index(user_index:dict, overwrite:bool=False):
+    user_initials = get_users('initial', user_index=user_index)
+    if not all(user_initials):
+        ini_to_name,namekey = create_default_initials(
+            get_users('fname', user_index=user_index), 
+            get_users('dname', user_index=user_index), 
+            get_users('uname', user_index=user_index)
+        )
+        
+        ud = get_users(by=namekey, user_index=user_index)
+        
+        for ini,name in ini_to_name.items():
+            # if not ud[name]['authorInitial']:
+            ud[name]['authorInitial'] = ini
+
+    
+    if USERS_FILEPATH.exists() and not overwrite:
+        raise FileExistsError(f'{USERS_FILEPATH} exists and overwrite=False')
+    
+    with open(USERS_FILEPATH, 'w') as f:
+        json.dump(user_index, f, indent=2)
+        print('Created users file at:', str(USERS_FILEPATH))
 
 def placeholder_userdata(populate=False):
-    user_data = {
-        "BOT": {"id": 0, "username": "cloneus", "displayName": "Cloneus", "isBot": True},
-        "USERS": []
-    }
+    user_index = [
+         {"id": 0, "firstName": "Cloney","authorInitial": "b0", "username": "cloneus", "displayName": "Cloneus", "isBot": True},
+    ]
     if populate:
         import string
         for i in range(1,8):
@@ -42,8 +63,8 @@ def placeholder_userdata(populate=False):
                 "isBot": False
             }
 
-            user_data["USERS"].append(placeholder_user)
-    return user_data
+            user_index.append(placeholder_user)
+    return user_index
 
     
 
@@ -51,12 +72,12 @@ def placeholder_userdata(populate=False):
 # if k and no v: return dict of dicts indexed by k where values are each user's whole data dict (including k)
 # if v and no k: return list of values, all user's v in no particular order
 # if neither k nor v: return list of dicts of all user's data dicts
-@functools.cache
+# @functools.cache
 def get_users(get: typing.Literal['dname','initial','fname','uname','bot','id']|None = None, 
               by: typing.Literal['dname','initial','fname','uname','id']|None = None, 
-              *, include_bot:bool=False
+              *, include_bot:bool=False, user_index: dict = None,
               ) -> dict[str,] | dict[str, dict[str,]] | list | list[dict[str,]]:
-    '''Reindex and filter user data.
+    '''Reindex and filter user data index.
 
     aliases: dname -> displayName; initial -> authorInitial; fname -> firstName; uname -> username; bot -> isBot
 
@@ -64,40 +85,42 @@ def get_users(get: typing.Literal['dname','initial','fname','uname','bot','id']|
         get: The value to select from each user's data. If None, return unfiltered user data
         by: The key to index user data by. If None, return a list instead of a dict
         include_bot: If True, include Cloneus bot as first entry in the returned data
+        user_index: The user data index to use. If None, will use global default (config/users.json) 
 
     Returns:
-        dict: { by : userdata[get] } if both `by` and `get`
-        dict[dict]: { by : userdata } if `by` and not `get`
-        list: userdata[get] if `get` and not `by`
-        list[dict]: userdata if neither `get` nor `by`
+        dict: { by : user_index[get] } if both `by` and `get`
+        dict[dict]: { by : user_index } if `by` and not `get`
+        list: user_index[get] if `get` and not `by`
+        list[dict]: user_index if neither `get` nor `by`
 
     Raises:
         ValueError: if `by` has missing or duplicate values
     '''
     
-    global USER_DATA
+    if user_index is None:
+        global USER_INDEX
+        
+        if USER_INDEX is None:
+            USER_INDEX = _read_user_index()
     
-    if USER_DATA is None:
-        USER_DATA = _read_userdata()
+        user_index = USER_INDEX
     
-    user_data = USER_DATA["USERS"]
     
-    if include_bot:
-        bot_data = {"firstName":'BottyMcBotface', "authorInitial":'b', **USER_DATA["BOT"]}
-        user_data = [bot_data, *user_data]
+    if not include_bot:
+        user_index = user_index[1:]
+    else:
+        assert user_index[0]['isBot'], 'Cloneus bot not in user data!'
+        user_index = user_index[:] # shallow copy just for consistency with exclude
+        #user_index = [bot_data, *user_index[1:]]
+        
     
     alias = {'dname':'displayName', 'initial':'authorInitial', 'fname':'firstName', 'uname':'username', 'bot':'isBot', 'id':'id'}
     
-    # I know I'm going to try pluralize it, just save myself the headache
-    if by:
-        by = by.rstrip('s')
-    if get:
-        get = get.rstrip('s')
 
     if by:
         # this could go later, but it *feels* better earlier
         _key = alias.get(by, by)
-        _vals = [u[_key] for u in user_data]
+        _vals = [u[_key] for u in user_index]
         if not all(_vals):
             raise ValueError(f'Cannot index by {by!r}. Not all users assigned a value for {by!r}.')
         if len(set(_vals)) != len(_vals):
@@ -105,52 +128,24 @@ def get_users(get: typing.Literal['dname','initial','fname','uname','bot','id']|
 
 
     if by is None and get is None:
-        return user_data
+        return user_index
     
     if get and not by:
         val = alias.get(get, get)
-        return [u[val] for u in user_data]
+        return [u[val] for u in user_index]
 
     
     key = alias.get(by, by)
 
     if by and not get:
-        return {u[key]: u for u in user_data}
+        return {u[key]: u for u in user_index}
     
     # idx_by and select
     val = alias.get(get, get)
-    return {u[key]: u[val] for u in user_data}
+    return {u[key]: u[val] for u in user_index}
 
 
 
-# def _load_author_helpers():
-#     USER_DATA = _read_userdata()
-#     author_display_names = []
-#     author_to_fname = {} # May have nulls if no firstName
-#     initial_to_author = {} # may have duplicate drops if no authorInitial
-#     author_to_id = {}
-    
-#     for u in USER_DATA['USERS']:
-#         author = u['displayName']
-#         author_display_names.append(author)
-#         author_to_fname[author] = u['firstName']
-#         author_to_id[author] = u['id']
-
-#         if u['authorInitial']:
-#             initial_to_author[u['authorInitial'].lower()] = author
-    
-#     # If initials are not provided, assign using first names if available
-#     # Otherwise, use display names for initials
-#     # TODO: allow partial initial assignment. 
-
-#     if not initial_to_author:
-#         fnames = list(author_to_fname.values())
-#         if all(fnames):
-#             initial_to_author = default_initials(fnames)
-#         else:
-#             initial_to_author = default_initials(author_display_names)
-
-#     return USER_DATA, author_display_names, author_to_fname, author_to_id, initial_to_author
 
 # Consts
 # author_display_names : get_users('dname')
@@ -189,16 +184,17 @@ def to_jinja_template(tag_sep:str, postfix:str):
     template = template.replace('__TAG_SEP__',tag_sep).replace('__POSTFIX__', postfix)
     return template
 
-def check_author_initials():
-    i_to_dname = get_users('dname', by='initial')
-    if i_to_dname:
-        assert len(set([i.lower() for i in i_to_dname])) == len(i_to_dname), 'Each user must be assigned a unique, case-insensitive initial or char+num(s).'
+def check_author_initials(user_initials:list[str] = None):
+    if user_initials is None:
+        user_initials = get_users('initial')
+    assert len(set([i.lower() for i in user_initials])) == len(user_initials), 'Each user must be assigned a unique, case-insensitive initial or char+num(s).'
 
 def parse_initials(initials_seq:str, return_dispname=False):
     '''aBC|A,b,c|a B c -> [a,b,c]. a2b1c1d11 -> [a2,b1,c1,d11]'''
     initials = re.findall(r'([a-z]\d*)[, ]?', initials_seq, re.I)
     if not return_dispname:
         return [i.lower() for i in initials]
+    
     i_to_dname = get_users('dname', 'initial')
     return [i_to_dname[i] for i in initials]
     
@@ -224,32 +220,51 @@ def assign_initials(names:list[str]):
 # If initials are not provided, assign using first names if available
 # Otherwise, use display names for initials
 # TODO: allow partial initial assignment. 
-def default_initials():
-    '''Get initials for each user
+def create_default_initials(first_names:list[str]=None, display_names:list[str]=None, usernames:list[str] = None):
+    '''Get initials for each user based name
+    
     priorty: 
         1: unique initial first names
         2: unique initial display names
-        3: first names
-        4: display names
-
-    Give priority to first names, if set.'''
-    fnames = get_users('fname')
-    dnames = get_users('dname')
+        3: unique initial usernames
+        4: first names
+        5: display names
+        6: usernames
+    '''
     
+    if first_names is None:
+        first_names = []
+    if display_names is None:
+        display_names = []
+    if usernames is None:
+        usernames = []
+
     i_to_fname = {}
     i_to_dname = {}
+    i_to_uname = {}
 
-    if all(fnames):
-        i_to_fname = assign_initials(fnames)
+    if any(first_names):
+        i_to_fname = assign_initials(first_names)
         if ''.join(i_to_fname.keys()).isalpha():
-            return i_to_fname
+            return i_to_fname,'fname'
         
-    if all(dnames):
-        i_to_dname = assign_initials(dnames)
+    if any(display_names):
+        i_to_dname = assign_initials(display_names)
         if ''.join(i_to_dname.key()).isalpha():
-            return i_to_dname
-        
-    if i_to_fname:
-        return i_to_fname
+            return i_to_dname,'dname'
     
-    return i_to_dname
+    if any(usernames):
+        i_to_uname = assign_initials(usernames)
+        if ''.join(i_to_uname.key()).isalpha():
+            return i_to_uname,'uname'
+                
+    if i_to_fname:
+        return i_to_fname, 'fname'
+    
+    if i_to_dname:
+        return i_to_dname, 'dname'
+    
+    if i_to_uname:
+        return i_to_uname, 'uname'
+    
+    raise ValueError('Need to specify at least one of `first_names`, `display_names`, or `usernames`')
