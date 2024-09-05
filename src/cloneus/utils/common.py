ba04ba8c706e0bb064a5b0f86fef72965258b508
Rypo <ryan@gust.dev>
2024-09-05 13:56:06 -0500

    fix: move batched util to common

--- src/cloneus/utils/common.py
+++ src/cloneus/utils/common.py
@@ -1,5 +1,6 @@
 import gc
 import time
+import itertools
 from contextlib import contextmanager
 
 import torch
@@ -18,3 +19,14 @@ def timed(label:str=''):
     finally:
         te = time.perf_counter()
         print(f'{label} : {te-t0:0.2f}s')
+
+
+def batched(iterable, n:int):
+    '''https://docs.python.org/3/library/itertools.html#itertools.batched'''
+    # batched('ABCDEFG', 3) → ABC DEF G
+    
+    if n < 1:
+        raise ValueError('n must be at least one')
+    iterator = iter(iterable)
+    while batch := list(itertools.islice(iterator, n)):
+        yield batch
